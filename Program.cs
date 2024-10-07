88f1af51a4cdcf3f6aff4ecd220139ef554b9988
kyotometus <kyotometus@pm.me>
2024-10-07 12:22:01 +0100

    gtksharp in the game

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Gtk;
+using System;
+
+class SimpleWindow : Window
+{
+    public SimpleWindow() : base("Simple GtkSharp Window")
+    {
+        SetDefaultSize(400, 200);
+        SetPosition(WindowPosition.Center);
+        DeleteEvent += delegate { Application.Quit(); };
+
+        Label label = new Label("Hello, GtkSharp!");
+        Add(label);
+
+        ShowAll();
+    }
+
+    static void Main()
+    {
+        Application.Init();
+        new SimpleWindow();
+        Application.Run();
+    }
+}
